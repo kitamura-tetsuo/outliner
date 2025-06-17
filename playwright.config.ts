7c4c0206ce185c8fcf63a8885f0457387dd80354
tetsuo <kitamura.tetsuo@gmail.com>
2025-06-17 12:53:46 +0900

    chore: update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,21 +1,23 @@
 /// <reference types="@playwright/test" />
 import { defineConfig } from "@playwright/test";
 
+const PORT = 7090;
+
 export default defineConfig({
-    testDir: "./tests",
+    testDir: "./client/e2e",
     timeout: 30 * 1000,
     expect: {
         timeout: 5000,
     },
-    // webServer: {
-    //     command: "npm run dev",
-    //     port: 5173,
-    //     reuseExistingServer: true,
-    // },
+    webServer: {
+        command: "npm --prefix client run dev",
+        port: PORT,
+        reuseExistingServer: true,
+    },
     use: {
         headless: true,
         viewport: { width: 1280, height: 720 },
         actionTimeout: 0,
-        baseURL: "http://localhost:5173",
+        baseURL: `http://localhost:${PORT}`,
     },
 });
