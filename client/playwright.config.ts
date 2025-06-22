aa7addd8016213c4b1241669ef0df333b4c16e64
tetsuo <kitamura.tetsuo@gmail.com>
2025-06-22 09:05:16 +0900

    Add new Playwright project for experimental tests

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -79,6 +79,12 @@ export default defineConfig({
             testDir: "./e2e/utils",
             use: { ...devices["Desktop Chrome"] },
         },
+        {
+            // 新機能テスト
+            name: "new",
+            testDir: "./e2e/new",
+            use: { ...devices["Desktop Chrome"] },
+        },
     ],
     // webServer: {
     //     command: `npx dotenv -e .env.test -- npm run dev -- --host 0.0.0.0 --port ${TEST_PORT}`,
