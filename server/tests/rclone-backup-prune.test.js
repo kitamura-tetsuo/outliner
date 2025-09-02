2041f5e0c8dcf31520717bf0d4216d5b74cdbca1
tetsuo <kitamura.tetsuo@gmail.com>
2025-09-01 20:00:51 -0700

    Validate backup retention days

--- server/tests/rclone-backup-prune.test.js
+++ server/tests/rclone-backup-prune.test.js
@@ -14,17 +14,30 @@ describe("rclone backup pruneOldBackups", function() {
         await fs.writeFile(oldFile, "old");
         const past = Date.now() - 10 * 24 * 60 * 60 * 1000;
         await fs.utimes(oldFile, past / 1000, past / 1000);
+        await fs.writeFile(path.join(backups, "new.tar.gz"), "new");
         process.env.BACKUP_DIR = backups;
     });
 
     afterEach(async () => {
         await fs.remove(temp);
         delete process.env.BACKUP_DIR;
+        delete process.env.BACKUP_RETENTION_DAYS;
     });
 
     it("removes backups older than retention", async function() {
         await pruneOldBackups(7);
-        const exists = await fs.pathExists(path.join(backups, "old.tar.gz"));
-        expect(exists).to.be.false;
+        const oldExists = await fs.pathExists(path.join(backups, "old.tar.gz"));
+        const newExists = await fs.pathExists(path.join(backups, "new.tar.gz"));
+        expect(oldExists).to.be.false;
+        expect(newExists).to.be.true;
+    });
+
+    it("defaults to 7 days for invalid BACKUP_RETENTION_DAYS", async function() {
+        process.env.BACKUP_RETENTION_DAYS = "not-a-number";
+        await pruneOldBackups();
+        const oldExists = await fs.pathExists(path.join(backups, "old.tar.gz"));
+        const newExists = await fs.pathExists(path.join(backups, "new.tar.gz"));
+        expect(oldExists).to.be.false;
+        expect(newExists).to.be.true;
     });
 });
