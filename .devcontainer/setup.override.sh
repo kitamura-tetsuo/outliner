0ad1304a4aead3d79aaef29a188f5f8f38a458b4
Tetsuo Kitamura <kitamura.tetsuo@gmail.com>
2025-11-21 00:40:36 +0900

    update dev containers

--- .devcontainer/setup.override.sh
+++ .devcontainer/setup.override.sh
@@ -1,15 +1,27 @@
 git config --global user.name "Tetsuo Kitamura"
 git config --global user.email kitamura.tetsuo@gmail.com
+git config --global pull.ff only
 
-/workspace/scripts/setup.sh
+curl -LsSf https://astral.sh/uv/install.sh | sh
 
-sudo apt update && sudo apt install -y pipx
-pipx ensurepath   # 必要に応じてシェルを再起動/再ログイン
+sudo apt update && sudo apt install -y pipx gh docker-compose-plugin pre-commit
+pipx ensurepath
 pipx install git+https://github.com/kitamura-tetsuo/auto-coder.git
 auto-coder --help
 
-npm install -g @qwen-code/qwen-code
-cd ~ && git clone https://github.com/kitamura-tetsuo/gemini-cli && cd gemini-cli && npm install && npm run build && npm install -g .
-npm install -g @openai/codex
+pipx install git+https://github.com/kitamura-tetsuo/github-sub-issue.git --force
+
+cd ~ && rm -rf gemini-cli && git clone https://github.com/kitamura-tetsuo/gemini-cli ; cd gemini-cli && git pull && npm install && npm run bundle && npm link && gemini -h
+
+npm install -g @anthropic-ai/claude-code
 npm install -g @augmentcode/auggie
+npm install -g @openai/codex
+npm install -g @qwen-code/qwen-code
+
+cd /workspace
+pre-commit install
+cd /workspaces/auto-coder
+pre-commit install
+
+# /workspace/scripts/setup.sh
 
