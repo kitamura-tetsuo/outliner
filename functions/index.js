265cc9b31c6b4704029aa0d64690e1150fb2e38b
GitHub Action <action@github.com>
2025-12-12 00:10:58 +0000

    Auto-fix: Format code with dprint

--- functions/index.js
+++ functions/index.js
@@ -1683,7 +1683,8 @@ exports.adminCheckForContainerUserListing = onRequest(
   },
 );
 
-exports.permanentlyDeleteProject = require("./src/permanentlyDeleteProject").permanentlyDeleteProject;
+exports.permanentlyDeleteProject =
+  require("./src/permanentlyDeleteProject").permanentlyDeleteProject;
 
 // 管理者ユーザーリスト API
 exports.adminUserList = onRequest(
