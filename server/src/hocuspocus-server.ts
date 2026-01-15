e9133a0217b173dae250958647402d8a1deeef2c
GitHub Action <action@github.com>
2026-01-15 23:51:21 +0000

    Auto-fix: Format code with dprint

--- server/src/hocuspocus-server.ts
+++ server/src/hocuspocus-server.ts
@@ -1,11 +1,11 @@
-import { Server } from '@hocuspocus/server'
-import { Logger } from '@hocuspocus/extension-logger'
+import { Logger } from "@hocuspocus/extension-logger";
+import { Server } from "@hocuspocus/server";
 
 // TODO: Get port from config
-const port = 1234
+const port = 1234;
 
 export const hocuspocus = Server.configure({
-  name: 'hocuspocus-fluid-outliner',
-  port,
-  extensions: [new Logger()]
-})
+    name: "hocuspocus-fluid-outliner",
+    port,
+    extensions: [new Logger()],
+});
