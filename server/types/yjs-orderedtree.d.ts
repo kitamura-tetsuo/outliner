ae429b88c80cfb44a1db4d34deefe7f88be53bff
GitHub Action <action@github.com>
2026-01-17 05:45:43 +0000

    Auto-fix: Format code with dprint

--- server/types/yjs-orderedtree.d.ts
+++ server/types/yjs-orderedtree.d.ts
@@ -1,10 +1,10 @@
-declare module 'yjs-orderedtree' {
-  import * as Y from 'yjs';
+declare module "yjs-orderedtree" {
+    import * as Y from "yjs";
 
-  export class OrderedTree {
-    constructor(doc: Y.Doc, map: Y.Map<any>);
-    // Add other methods as needed based on usage
-    observe(callback: (event: any) => void): void;
-    observeDeep(callback: (event: any) => void): void;
-  }
+    export class OrderedTree {
+        constructor(doc: Y.Doc, map: Y.Map<any>);
+        // Add other methods as needed based on usage
+        observe(callback: (event: any) => void): void;
+        observeDeep(callback: (event: any) => void): void;
+    }
 }
