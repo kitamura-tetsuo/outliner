5e29e0bb51ca83e4244b928950be6e9f78034efa
Tetsuo Kitamura <kitamura.tetsuo@gmail.com>
2026-01-25 14:00:59 +0900

    refactor: remove duplicate coverage hook registration

--- client/e2e/basic/project-deletion.spec.ts
+++ client/e2e/basic/project-deletion.spec.ts
@@ -1,11 +1,8 @@
 import { registerCoverageHooks } from "../utils/registerCoverageHooks";
 registerCoverageHooks();
 import { expect, test } from "@playwright/test";
-import { registerCoverageHooks } from "../utils/registerCoverageHooks";
 import { SeedClient } from "../utils/seedClient";
 import { TestHelpers } from "../utils/testHelpers";
-
-registerCoverageHooks();
 
 test.describe("Project Deletion", () => {
     test("should allow deleting a project", async ({ page }, testInfo) => {
