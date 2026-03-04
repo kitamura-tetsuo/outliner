2da338396dad5203b974c57dad047702cb2b0f3f
GitHub Action <action@github.com>
2026-03-04 14:06:02 +0000

    Auto-fix: Format code with dprint

--- patch.cjs
+++ patch.cjs
@@ -1,4 +1,7 @@
-const fs = require('fs');
-const content = fs.readFileSync('client/src/lib/Cursor.ts', 'utf8');
-const fixed = content.replace(/<<<<<<< HEAD[\s\S]*?=======\s*(\/\/ eslint-disable-next-line @typescript-eslint\/no-explicit-any\s*if \(typeof \(store as any\)\.forceUpdate === 'function'\) \{\s*\/\/ eslint-disable-next-line @typescript-eslint\/no-explicit-any\s*)>>>>>>> 9404272 \(fix: resolve any usage in Cursor\.ts\)\s*(\(store as any\)\.forceUpdate\(\);\s*\})/m, '$1$2');
-fs.writeFileSync('client/src/lib/Cursor.ts', fixed);
+const fs = require("fs");
+const content = fs.readFileSync("client/src/lib/Cursor.ts", "utf8");
+const fixed = content.replace(
+    /<<<<<<< HEAD[\s\S]*?=======\s*(\/\/ eslint-disable-next-line @typescript-eslint\/no-explicit-any\s*if \(typeof \(store as any\)\.forceUpdate === 'function'\) \{\s*\/\/ eslint-disable-next-line @typescript-eslint\/no-explicit-any\s*)>>>>>>> 9404272 \(fix: resolve any usage in Cursor\.ts\)\s*(\(store as any\)\.forceUpdate\(\);\s*\})/m,
+    "$1$2",
+);
+fs.writeFileSync("client/src/lib/Cursor.ts", fixed);
