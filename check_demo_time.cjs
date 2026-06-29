ef33c19fe7d92609fd18a8f88f31d093e1621431
GitHub Action <action@github.com>
2026-06-29 04:25:44 +0000

    Auto-fix: Format code with dprint

--- check_demo_time.cjs
+++ check_demo_time.cjs
@@ -1,13 +1,13 @@
-const { chromium } = require('playwright');
+const { chromium } = require("playwright");
 (async () => {
     const browser = await chromium.launch();
     const page = await browser.newPage();
     const startTime = Date.now();
-    page.on('console', msg => {
+    page.on("console", msg => {
         console.log(`[${Date.now() - startTime}ms] Console [${msg.type()}]:`, msg.text());
     });
 
-    await page.goto('https://outliner-d57b0.web.app/demo');
+    await page.goto("https://outliner-d57b0.web.app/demo");
     await page.waitForTimeout(35000);
     await browser.close();
 })();
