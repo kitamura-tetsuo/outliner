994fed3e00c835c1ddc0c44e067e940ee1bd1058
GitHub Action <action@github.com>
2026-07-04 02:26:50 +0000

    Auto-fix: Format code with dprint

--- patch_keh10.cjs
+++ patch_keh10.cjs
@@ -1,5 +1,8 @@
-const fs = require('fs');
-const file = 'client/src/lib/KeyEventHandler.ts';
-let code = fs.readFileSync(file, 'utf8');
-code = code.replace(/        const k = \(event as KeyboardEvent\)\.key;\n\n        \/\/ Command Palette Interaction/g, '        // Command Palette Interaction');
+const fs = require("fs");
+const file = "client/src/lib/KeyEventHandler.ts";
+let code = fs.readFileSync(file, "utf8");
+code = code.replace(
+    /        const k = \(event as KeyboardEvent\)\.key;\n\n        \/\/ Command Palette Interaction/g,
+    "        // Command Palette Interaction",
+);
 fs.writeFileSync(file, code);
