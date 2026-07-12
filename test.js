fc68cda915b3ec3e56f4de04a612700f93ed26d4
GitHub Action <action@github.com>
2026-07-12 13:21:23 +0000

    Auto-fix: Format code with dprint

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import { render } from 'svelte/server';
+import { render } from "svelte/server";
 // Wait, I can't easily test Svelte 5 compilation.
 // Svelte 5 rule:
 // if (value == null) element.removeAttribute(key)
