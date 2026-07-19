c74898c80a1ca9ce5b028a294a595e73bad745a9
GitHub Action <action@github.com>
2026-07-19 13:55:03 +0000

    Auto-fix: Format code with dprint

--- server/tests/demo-api.spec.ts
+++ server/tests/demo-api.spec.ts
@@ -5,8 +5,8 @@ import * as Y from "yjs";
 import { YTree } from "yjs-orderedtree";
 import { createDemoRouter } from "../src/demo-api.js";
 import { DEMO_PROJECT_TITLE, DEMO_TEMPLATE_VERSION } from "../src/demo-content.js";
-import { Project } from "../src/schema/app-schema.js";
 import { populateDemoProject } from "../src/demo-content.js";
+import { Project } from "../src/schema/app-schema.js";
 
 describe("Demo API", () => {
     let mockHocuspocus: any;
