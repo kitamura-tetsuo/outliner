c478c58b778aa1ff91a65d9572b08426bae4d30a
GitHub Action <action@github.com>
2026-07-22 18:22:58 +0000

    Auto-fix: Format code with dprint

--- client/e2e/core/item-movement.spec.ts
+++ client/e2e/core/item-movement.spec.ts
@@ -14,7 +14,8 @@ test.describe("Item Movement and Focus", () => {
         await page.locator('[role="tree"]').focus();
 
         const focusableCount = await page.evaluate(() => {
-            const sel = 'a[href], button:not([disabled]), input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"])';
+            const sel =
+                'a[href], button:not([disabled]), input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"])';
             return document.querySelector('[role="tree"]').querySelectorAll(sel).length;
         });
 
