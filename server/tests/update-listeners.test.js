ffb18cb1afb5d508466a0208d3d50df02f88d1f9
GitHub Action <action@github.com>
2026-07-22 17:38:18 +0000

    Auto-fix: Format code with dprint

--- server/tests/update-listeners.test.js
+++ server/tests/update-listeners.test.js
@@ -1,8 +1,8 @@
 // Basic placeholder test for room size listener since it's referenced but missing
-import { expect } from 'chai';
+import { expect } from "chai";
 
-describe('update-listeners', () => {
-    it('should have a placeholder for room size listener migration', () => {
+describe("update-listeners", () => {
+    it("should have a placeholder for room size listener migration", () => {
         expect(true).to.be.true;
     });
 });
