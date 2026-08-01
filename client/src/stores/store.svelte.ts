89569d60047acb1e868a6b50951d743626bc2ebc
GitHub Action <action@github.com>
2026-08-01 01:37:46 +0000

    Auto-fix: Format code with dprint

--- client/src/stores/store.svelte.ts
+++ client/src/stores/store.svelte.ts
@@ -10,8 +10,8 @@ import { saveProjectSnapshot } from "../lib/projectSnapshot";
 import type { Items } from "../schema/app-schema";
 import { Item, Project } from "../schema/app-schema";
 import { globalUndoRouter } from "../services/undo/undoRouter";
-import { CHECKBOX_ROLLUP_ORIGIN, updateParentCheckboxStatus } from "../utils/checkboxHelpers";
 import { ITEMS_RELATION_ORIGIN } from "../services/yjstable/itemsRelation";
+import { CHECKBOX_ROLLUP_ORIGIN, updateParentCheckboxStatus } from "../utils/checkboxHelpers";
 
 export class GeneralStore {
     // Use $state for pages to ensure proper Svelte reactivity
@@ -297,7 +297,9 @@ export class GeneralStore {
             this.undoManager.destroy();
         }
         /* eslint-disable svelte/prefer-svelte-reactivity -- Y.UndoManager internally uses standard Set which is required by its API */
-        this.undoManager = new Y.UndoManager(v.ydoc.getMap("orderedTree"), { trackedOrigins: new Set([null, ITEMS_RELATION_ORIGIN]) });
+        this.undoManager = new Y.UndoManager(v.ydoc.getMap("orderedTree"), {
+            trackedOrigins: new Set([null, ITEMS_RELATION_ORIGIN]),
+        });
         globalUndoRouter.register(this.undoManager);
         /* eslint-enable svelte/prefer-svelte-reactivity */
 
